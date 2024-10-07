07668acd77d93707f909a828c33f5f6199cef710
Muhamad Tarmowijaya <m.tarmowijaya@gmail.com>
2024-10-07 08:53:26 +0700

    rev():navbar and content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,57 @@
 
 @section('content')
     <div class="ms-4">
-        <h1>Dashboard</h1>
+        <div class="card">
+            <div class="card-header">
+                <h1>Dashboard</h1>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="feather-user fs-4 text-primary"></i>
+                                <h6 class="mt-4">Users</h6>
+                                <p class="fs-11 my-3">123</p>
+                                <a href="#" class="btn btn-primary w-100">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="feather-user fs-4 text-primary"></i>
+                                <h6 class="mt-4">Users</h6>
+                                <p class="fs-11 my-3">123</p>
+                                <a href="#" class="btn btn-primary w-100">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="feather-user fs-4 text-primary"></i>
+                                <h6 class="mt-4">Users</h6>
+                                <p class="fs-11 my-3">123</p>
+                                <a href="#" class="btn btn-primary w-100">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="feather-user fs-4 text-primary"></i>
+                                <h6 class="mt-4">Users</h6>
+                                <p class="fs-11 my-3">123</p>
+                                <a href="#" class="btn btn-primary w-100">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Tambahkan elemen lain sesuai dengan template -->
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
     <!-- Tambahkan elemen konten lain sesuai dengan template -->
 @endsection
